8dcea52214af1b1f91c91438a6e3fd2389d45304
Brett Jones <brettjonesdev@gmail.com>
2015-12-31 13:47:34 -0600

    Fix navbar, change `class` to `className` for all attributes to avoid `Warning: Unknown DOM property class. Did you mean className?`

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -2,11 +2,15 @@ import React from 'react';
 import {Link} from 'react-router';
 
 export default class Nav extends React.Component {
-  render() {
-    return (
-      <nav className="navbar navbar-default">
-        <div><Link to="/">GameTracker</Link></div>
-      </nav>
-    );
-  }
+    render() {
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <Link to="/" className="navbar-brand">GameTracker</Link>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
 }
